0f2384459a28674fe96d04a51c9595ca0caba649
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2022-01-19 10:58:13 -0800

    first commit with spring boot project structure

---
+++ src/main/java/cyjimmy/travelidea/TravelideaApplication.java
@@ -0,0 +1,13 @@
+package cyjimmy.travelidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelideaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cyjimmy/travelidea/TravelideaApplicationTests.java
@@ -0,0 +1,13 @@
+package cyjimmy.travelidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelideaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
